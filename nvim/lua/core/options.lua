6b77db11affaed1b4541c49b2102f15ac011324d
dracarys18 <me@karthihegde.dev>
2026-08-28 12:46:03 +0530

    update

--- nvim/lua/core/options.lua
+++ nvim/lua/core/options.lua
@@ -1,43 +1,43 @@
-vim.g.mapleader      = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader            = ' '
+vim.g.maplocalleader       = ' '
 
 -- line numbers
-vim.o.number         = true
-vim.o.relativenumber = false
+vim.o.number               = true
+vim.o.relativenumber       = false
 
 -- timing
-vim.o.timeoutlen     = 700
+vim.o.timeoutlen           = 700
 
 -- gui
-vim.o.guifont        = 'Hasklug Nerd Font Mono,Hack Nerd Font,NotoEmoji Nerd Font:h11'
+vim.o.guifont              = 'Hasklug Nerd Font Mono,Hack Nerd Font,NotoEmoji Nerd Font:h11'
 
 -- undo
-vim.o.undodir        = vim.fn.stdpath('cache') .. '/undodir'
-vim.o.undofile       = true
+vim.o.undodir              = vim.fn.stdpath('cache') .. '/undodir'
+vim.o.undofile             = true
 
 -- editing
-vim.o.autoread       = true
-vim.o.foldmethod     = 'indent'
-vim.o.showmode       = false
-vim.o.showtabline    = 0
-vim.o.autoindent     = true
-vim.o.tabstop        = 4
-vim.o.softtabstop    = 4
-vim.o.shiftwidth     = 4
-vim.o.expandtab      = true
-vim.o.hidden         = true
-vim.o.wrap           = false
-vim.o.clipboard      = 'unnamedplus'
-vim.o.completeopt    = 'menuone,noselect,popup'
+vim.o.autoread             = true
+vim.o.foldmethod           = 'indent'
+vim.o.showmode             = false
+vim.o.showtabline          = 0
+vim.o.autoindent           = true
+vim.o.tabstop              = 4
+vim.o.softtabstop          = 4
+vim.o.shiftwidth           = 4
+vim.o.expandtab            = true
+vim.o.hidden               = true
+vim.o.wrap                 = false
+vim.o.clipboard            = 'unnamedplus'
+vim.o.completeopt          = 'menuone,noselect,popup'
 
 -- search
-vim.o.ignorecase     = true
-vim.o.smartcase      = true
+vim.o.ignorecase           = true
+vim.o.smartcase            = true
 
 -- ui
-vim.o.termguicolors  = true
-vim.wo.signcolumn    = 'yes'
-vim.opt.list         = true
+vim.o.termguicolors        = true
+vim.wo.signcolumn          = 'yes'
+vim.opt.list               = true
 
 -- zig
 vim.g.zig_fmt_parse_errors = 0
@@ -47,4 +47,20 @@ vim.g.zig_fmt_autosave     = 0
 vim.g.python_highlight_all = 1
 
 -- Autoread after buffer refreshed
-vim.o.autoread = true
+vim.o.autoread             = true
+local group                = vim.api.nvim_create_augroup("AutoRead", { clear = true })
+
+vim.api.nvim_create_autocmd(
+    { "FocusGained", "TermClose", "TermLeave", "BufEnter", "CursorHold" },
+    {
+        group = group,
+        command = "checktime",
+    }
+)
+
+vim.api.nvim_create_autocmd("FileChangedShellPost", {
+    group = group,
+    callback = function()
+        vim.notify("File reloaded from disk")
+    end,
+})
--- nvim/lua/keymaps.lua
+++ nvim/lua/keymaps.lua
@@ -109,9 +109,10 @@ for idx = 1, #insert_mode_maps do
 end
 
 -- ── Trouble ───────────────────────────────────────────────────────────────────
-vim.keymap.set('n', '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>', { desc = "Buffer Diagnostics (Trouble)" })
-vim.keymap.set('n', '<leader>cs', '<cmd>Trouble symbols toggle focus=false<cr>',      { desc = "Symbols (Trouble)" })
-vim.keymap.set('n', '<leader>cl', '<cmd>Trouble lsp toggle focus=false win.position=right<cr>', { desc = "LSP Definitions / references" })
-vim.keymap.set('n', '<leader>xL', '<cmd>Trouble loclist toggle<cr>',                  { desc = "Location List (Trouble)" })
-vim.keymap.set('n', '<leader>xQ', '<cmd>Trouble qflist toggle<cr>',                  { desc = "Quickfix List (Trouble)" })
-
+vim.keymap.set('n', '<leader>xX', '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+    { desc = "Buffer Diagnostics (Trouble)" })
+vim.keymap.set('n', '<leader>cs', '<cmd>Trouble symbols toggle focus=false<cr>', { desc = "Symbols (Trouble)" })
+vim.keymap.set('n', '<leader>cl', '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+    { desc = "LSP Definitions / references" })
+vim.keymap.set('n', '<leader>xL', '<cmd>Trouble loclist toggle<cr>', { desc = "Location List (Trouble)" })
+vim.keymap.set('n', '<leader>xQ', '<cmd>Trouble qflist toggle<cr>', { desc = "Quickfix List (Trouble)" })
